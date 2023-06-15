a401f3519ffdf27e3e96ebe7f182851a03af0c31
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-15 13:31:11 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: print bt puts
  *
- * Return: 0
+ * Return: always :0
 */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzlei");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
