931d0be824eaadeb075205cf43c5998d77672099
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-16 15:20:33 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entey
+ *
+ * Description: Digits
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+
+	return (0);
+}
