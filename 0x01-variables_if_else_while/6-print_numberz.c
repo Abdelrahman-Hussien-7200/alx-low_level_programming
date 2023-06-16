3df9518b33357163f240a7c0ac896eea8c8ad1e8
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-16 15:33:02 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: Digits
+ *
+ * return: 0 success
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
