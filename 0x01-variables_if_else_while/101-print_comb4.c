9d2888de9ff6aab2ae9c2284ff3ea04f6bcbf499
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-17 05:05:55 -0700

    ss

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,44 +3,42 @@
 /**
  * main - Entry
  *
- * Description: Nested 3 loops
- *
+ * Description : Nested loops
  * Return: 0 success
 */
 
 int main(void)
-{       
-        int x1;
-        int x2;
-        int x3;
-        
-        for (x1 = 48; x1 <= 57; x1++)
-        {
-                x2 = 48;
-                for (; x2 <= 57; x2++)
-                {
-                    x3 =48;
-                        for (; x3 <= 57; x3++)
-                        {
-                        if (x1 != x2  && x1 < x2 )
-                        {
-                                if (x2 != x3 && x2 < x3)
-                                {putchar(x1);
-                                putchar(x2);
-                                putchar(x3);
-                                if (x1 + x2 +x3 != 55+ 56 + 57)
-                                {
-                                        putchar (44);
-                                        putchar (' ');
-                                }
-                                    
-                                }
-                        }
-                            
-                        }
-                }
-        }
-        putchar('\n');
+{
+	int x1;
+	int x2;
+	int x3;
 
-        return (0);
+	for (x1 = 48; x1 <= 57; x1++)
+	{
+		x2 = 48;
+		for (; x2 <= 57; x2++)
+		{
+			x3 = 48;
+			for (; x3 <= 57; x3++)
+			{
+				if (x1 != x2  && x1 < x2)
+				{
+					if (x2 != x3 && x2 < x3)
+						{
+							putchar(x1);
+							putchar(x2);
+							putchar(x3);
+							if (x1 + x2 + x3 != 55 + 56 + 57)
+							{
+								putchar (44);
+								putchar (' ');
+							}
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
