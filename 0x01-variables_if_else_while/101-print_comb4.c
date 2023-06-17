3b91d403e4507b4aaaeebeb1569140e037f5952a
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-17 05:51:03 -0700

    ss

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,30 +10,26 @@
 
 int main(void)
 {
-	int x1;
-	int x2;
-	int x3;
+	int x1, x2, x3;
 
 	for (x1 = 48; x1 <= 57; x1++)
 	{
-		x2 = 48;
-		for (; x2 <= 57; x2++)
+		for (x2 = 48; x2 <= 57; x2++)
 		{
-			x3 = 48;
-			for (; x3 <= 57; x3++)
+			for (x3 = 48; x3 <= 57; x3++)
 			{
 				if (x1 != x2  && x1 < x2)
 				{
 					if (x2 != x3 && x2 < x3)
 					{
-						putchar(x1);
-						putchar(x2);
-						putchar(x3);
-						if (x1 + x2 + x3 != 55 + 56 + 57)
-						{
-							putchar (44);
-							putchar (' ');
-						}
+					putchar(x1);
+					putchar(x2);
+					putchar(x3);
+					if (x1 + x2 + x3 != 55 + 56 + 57)
+					{
+					putchar (44);
+					putchar (' ');
+					}
 					}
 				}
 			}
