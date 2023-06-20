6facf428429288833bc9a49a30237f591f5619a5
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-20 13:13:42 -0700

    loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry
+ *
+ * @c: the char to be checked
+ *
+ * Description: Loops
+ *
+ * Return: 1 or 0
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 96 && c <= 122) && (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
