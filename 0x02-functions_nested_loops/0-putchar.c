574d2f459afd0810662e9b93f061bdb62be5f1f2
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-20 11:44:22 -0700

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: Nested loops
+ *
+ * Return: 0 seccuss
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
