726d3d74084b9d86286fd7fca0f221347667e8c5
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-20 14:38:26 -0700

    loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ *
+ * @i: integer
+ *
+ * @j: integer
+ *
+ * Description: Loops
+ *
+ * Return: add int
+*/
+
+int add(int i, int j)
+{
+	return (i + j);
+}
