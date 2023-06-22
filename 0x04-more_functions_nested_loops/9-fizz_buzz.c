7e17d52db682ae9d7377ebc0884a81c5c0429a02
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 09:33:22 -0700

    abdo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)	
 	{
 	if (i % 3 == 0 && i % 5 != 0)
 		printf("Fizz");
@@ -21,10 +21,11 @@ int main(void)
 	else if (i % 3 == 0 && i % 5 == 0)
 		printf("FizzBuzz");
 	else
-		printf("%d", i);
+		printf("%i", i);
 	if (i < 100)
 		printf(" ");
 	}
 
+	printf("\n");
 	return (0);
 }
