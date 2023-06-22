f11870432b3033e755785a12189cd942dabe7a54
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 08:43:50 -0700

    abdo

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - Entry
+ *
+ * @size: int
+ *
+ * Description: h
+ *
+ * Return: 0
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+		_putchar(35);
+		}
+		_putchar('\n');
+	}
+	}
+}
