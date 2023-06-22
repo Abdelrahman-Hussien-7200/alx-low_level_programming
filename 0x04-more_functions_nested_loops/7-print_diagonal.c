ad3fed79a0fb191ddd09e0e28f8e51959116bf5e
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 08:09:47 -0700

    abdo

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Entry
+ *
+ * @n: int
+ *
+ * Description: h
+ *
+ * Return: 0
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= n; i++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\');
+	_putchar('\n');
+	}
+}
