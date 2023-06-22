91403f044b40fa8bae1f6cdf77aed78254c10971
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 06:55:37 -0700

    abdo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry
+ *
+ * @a: int
+ *
+ * @b: int
+ *
+ * Description: h
+ *
+ * Return: 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
