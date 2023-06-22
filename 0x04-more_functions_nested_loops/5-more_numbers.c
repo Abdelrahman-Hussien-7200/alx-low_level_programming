4211a8392fc1b05400ffd4f08fe67cddfc4c18c0
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 07:22:20 -0700

    abdo

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - Entry
+ *
+ * Description: h
+ *
+ * Return: void
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+		_putchar(i + 48);
+	}
+	}
+	_putchar('\n');
+}
