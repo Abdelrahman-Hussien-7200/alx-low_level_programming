025278c85d10e034c7a2af761452ac1b900e12c6
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 07:07:41 -0700

    abdo

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry
+ *
+ * Description: h
+ *
+ * Return: void
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 || i != 52)
+		{
+		_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
