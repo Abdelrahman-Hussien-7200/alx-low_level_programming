8170383aca38d6b146eab918d886211bec881755
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 07:00:35 -0700

    abdo

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry
+ *
+ * Description: h
+ *
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('\n');
+	}
+}
