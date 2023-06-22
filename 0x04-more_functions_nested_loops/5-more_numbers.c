558a155e261c1ee84370c9d714e1a6b01cb10fb6
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-22 07:35:36 -0700

    abdo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,21 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, count;
 
 	for (j = 0; j < 10; j++)
 	{
 	for (i = 0; i <= 14; i++)
 	{
-		_putchar(i + 48);
-	}
+		count = i;
+
+		if (i > 9)
+		{
+			_putchar(1 + 48);
+			count = i % 10;
+		}
+		_putchar(count + 48);
 	}
 	_putchar('\n');
+	}
 }
