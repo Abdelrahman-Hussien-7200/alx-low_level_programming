359f3b70a8ed4623b0aebb314b0131dfbba867f4
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 12:53:47 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev- Entery
+ *
+ * Description: Don't swap horses in crossing a stream
+ *
+ * @s: char
+ *
+ * Return: 0
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (; s[i] != '\0'; i++)
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+		_putchar('\n');
+}
