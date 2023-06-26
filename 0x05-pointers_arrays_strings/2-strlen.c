1cba733a3bb4e1c5b026b7533f6b97b9da11d449
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 12:15:11 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Entery
+ *
+ * Description: Don't swap horses in crossing a stream
+ *
+ * @s: char
+ *
+ * @b: int
+ *
+ * Return: 0
+*/
+
+int _strlen(char *s)
+{
+	strlen(*s);
+}
