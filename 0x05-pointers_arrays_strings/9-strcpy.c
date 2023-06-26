d812a67bc8b8552471799edf8c1ab054852fbd5f
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 14:44:58 -0700

    p

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy- Entery
+ *
+ * Description: Don't swap horses in crossing a stream
+ *
+ * @dest: char
+ *
+ * @src: char
+ *
+ * Return: 0
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+	dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
