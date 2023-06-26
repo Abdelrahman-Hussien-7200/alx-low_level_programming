f88f7b565817b2500b6f2c06d99e9dec2229e939
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 12:06:25 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *swap_int - Entery
+ * 
+ *Description: Don't swap horses in crossing a stream
+ * 
+ * @a: int
+ *
+ * @b: int
+ *
+ * Return: 0
+*/
+
+void swap_int(int *a, int *b)
+{
+	int v;
+
+	*a = v;
+	*a = *b;
+	*b = v;
+}
