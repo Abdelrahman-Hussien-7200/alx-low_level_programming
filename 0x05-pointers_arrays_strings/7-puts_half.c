0eb68a0db642ef740b808ad9dc4864cd8bed9359
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 14:11:56 -0700

    p

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half- Entery
+ *
+ * Description: Don't swap horses in crossing a stream
+ *
+ * @str: char
+ *
+ * Return: 0
+*/
+
+void puts_half(char *str)
+{
+	int i, j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 1)
+		i = (j - 1) / 2;
+	else
+		i = j / 2;
+
+	for (; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
