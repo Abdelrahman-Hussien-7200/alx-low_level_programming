493fa543623662c763af191aaceb93870ef979da
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 13:53:13 -0700

    p

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,9 +13,14 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i, j = 0;
 
-	for (; str[i] != '\0'; i += 2)
+	while (str[j] != '\0')
+	{
+		j++
+	}
+
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 	}
