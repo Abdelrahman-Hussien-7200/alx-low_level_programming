1a757013abad6daadf36ca725e0dbcde550e2606
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 13:41:33 -0700

    p

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2- Entery
+ *
+ * Description: Don't swap horses in crossing a stream
+ *
+ * @str: char
+ *
+ * Return: 0
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
