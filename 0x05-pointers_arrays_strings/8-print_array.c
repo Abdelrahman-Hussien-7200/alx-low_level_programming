75639cc407666f32be854b235b45c47bd08f2511
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-26 14:25:18 -0700

    p

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array- Entery
+ *
+ * Description: Don't swap horses in crossing a stream
+ *
+ * @a: int
+ *
+ * @n: int
+ *
+ * Return: 0
+*/
+
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		printf("%d, ", a[j]);
+	}
+	printf("\n");
+
+}
