fcbf6d6e418cc1e09745204c6d8dd3403b8f00ef
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-29 07:57:30 -0700

    pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,22 +12,20 @@
 
 char *leet(char *n)
 {
-	int i;
+	int i, j;
+
+	char st1[] = "aAeEoOtTlL"
+	char st2[] = "4433007711"
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (n[i] == 'a' || n[i] == 'A')
-			n[i] = 4;
-		else if (n[i] == 'e' || n[i] == 'E')
-			n[i] = 3;
-		else if (n[i] == 'o' || n[i] == 'O')
-			n[i] = 0;
-		else if (n[i] == 't' || n[i] == 'T')
-			n[i] = 7;
-		else if (n[i] == 'l' || n[i] == 'L')
-			n[i] = 1;
-		else
-			n[i] = n[i];
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == st1[j])
+			{
+				n[i] = st2[j];
+			}
+		}
 	}
 	return (n);
 }
