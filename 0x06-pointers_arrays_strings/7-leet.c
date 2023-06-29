2e048f907a11c8eac4d5c1ae02e5e05dd10239f5
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-06-29 07:44:36 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * *leet - Entry
+ *
+ *@n:char
+ *
+ * Description: pointers
+ *
+ * Return:char
+*/
+
+char *leet(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] == 'a' || n[i] == 'A')
+			n[i] = 4;
+		else if (n[i] == 'e' || n[i] == 'E')
+			n[i] = 3;
+		else if (n[i] == 'o' || n[i] == 'O')
+			n[i] = 0;
+		else if (n[i] == 't' || n[i] == 'T')
+			n[i] = 7;
+		else if (n[i] == 'l' || n[i] == 'L')
+			n[i] = 1;
+		else
+			n[i] = n[i];
+	}
+	return (n);
+}
