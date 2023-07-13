adec7df635791f30e063d69d19d2a28e415f994b
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-13 14:01:09 -0700

    abdo

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,20 +13,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int v, j = 0;
+	unsigned int v, h;
 	char *i;
 
-	i = malloc(size * sizeof(nmemb));
+	h = nmemb + size;
+	i = malloc(h);
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (i == NULL)
 		return (NULL);
-
-	while (i[j])
-		j++;
-
-	for (v = 0; v < j; v++)
+	for (v = 0; v < h; v++)
 	{
 		i[v] = 0;
 	}
