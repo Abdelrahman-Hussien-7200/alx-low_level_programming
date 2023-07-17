3b7116125929222e6d9995f30a19e2bf1e42f773
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-17 11:20:20 -0700

    micro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry
+ *
+ * Description: file name
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	printf("File name is : %s\n", _FILE_);
+	return (0);
+}
