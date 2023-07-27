363c351274188f8d326c869ddd59a01834178edf
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-27 14:03:27 -0700

    abdo

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,54 +1,35 @@
 #include "lists.h"
 
 /**
-  * add_node - Adds a new node
-  *
-  * @head: single linked list
-  *
-  * @str: The string
-  *
-  * Return: The address of the new list or NULL if it failed
-*/
-
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to a pointer to the head of the list
+ * @str: string to be added to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
+	list_t *new_node;
 
-	if (head != NULL && str != NULL)
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
 	{
-		temp = malloc(sizeof(list_t));
-		if (temp == NULL)
-			return (NULL);
-
-		temp->str = strdup(str);
-		temp->len = _strlen(str);
-		temp->next = *head;
-
-		*head = temp;
-
-		return (temp);
+	free(new_node);
+	return (NULL);
 	}
 
-	return (0);
-}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
 
-/**
-  * _strlen - Returns the length
-  *
-  * @s: String
-  *
-  * Return: String length
-*/
-
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (*s)
-	{
-		s++;
-		i++;
-	}
-
-	return (i);
+	return (new_node);
 }
