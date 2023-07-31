8260fa607bd02f2e3796a88db14cfecaa8b2da22
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-31 13:07:04 -0700

    abdo

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -21,5 +21,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		i++;
 	}
 
-	return (current ? current : NULL);
+	if (current != NULL)
+		return (current);
+	else
+		return (NULL);
 }
