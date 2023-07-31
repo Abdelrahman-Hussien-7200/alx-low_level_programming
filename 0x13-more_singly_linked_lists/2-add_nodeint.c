b5f2c2bab72d171b4576239aaea033f3b162801e
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-31 11:43:04 -0700

    abdo

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint - adds a new node at the beginning of a list_t list
  * @head: pointer to a pointer to the head of the list
- * @str: string to be added to the new node
+ * @n: integer to be added to the new node
  *
  * Return: the address of the new element, or NULL if it failed
 */
