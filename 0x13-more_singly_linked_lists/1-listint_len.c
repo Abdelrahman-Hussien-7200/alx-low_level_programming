6b4112dca755987d1ed3f3c97f05b3af9617d09c
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-31 11:16:44 -0700

    abdo

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * list_len - entery point
+  *
+  * @h: A single linked list
+  *
+  * Return: The number of elements of a list
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * free_list - single linked list.
+  *
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: Nothing
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
+}
