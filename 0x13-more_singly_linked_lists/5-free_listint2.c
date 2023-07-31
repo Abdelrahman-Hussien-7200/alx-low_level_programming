238d7fcf5897f027a65396844eacb030199b221c
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-07-31 12:02:30 -0700

    abdo

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - single linked list.
+  *
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: Nothing
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
