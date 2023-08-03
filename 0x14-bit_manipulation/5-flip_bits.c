cf20308f47cdf82d1144fc5aed5932cf5cc7bc9b
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-08-03 07:29:32 -0700

    abdo

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,13 +3,15 @@
 /**
  * flip_bits - Entery point
  *
- * Description: a function that returns the number of bits you would need to flip to get from one number to another.
+ * Description: a function that returns the number of bits
+ * you would need to flip to get from one number to another.
  *
  * @n: unsigned long int
  *
  * @m: unsigned long int
  *
- * Return: the number of bits you would need to flip to get from one number to another
+ * Return: the number of bits you would
+ * need to flip to get from one number to another
 */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
