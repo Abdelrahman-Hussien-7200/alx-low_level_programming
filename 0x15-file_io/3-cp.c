db87e8d2fda9b40737b6c7f7bd08d706f2c5a0eb
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-08-07 13:51:33 -0700

    abdo

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,10 +4,12 @@ void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
+ *
  * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
- */
+*/
+
 char *create_buffer(char *file)
 {
 	char *buffer;
@@ -26,24 +28,28 @@ char *create_buffer(char *file)
 
 /**
  * close_file - Closes file descriptors.
+ *
  * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
+*/
+
+void close_file(int fdis)
 {
-	int c;
+	int i;
 
-	c = close(fd);
+	i = close(fdis);
 
-	if (c == -1)
+	if (i == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdis);
 		exit(100);
 	}
 }
 
 /**
  * main - Copies the contents of a file to another file.
+ *
  * @argc: The number of arguments supplied to the program.
+ *
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
@@ -52,7 +58,8 @@ void close_file(int fd)
  * If file_from does not exist or cannot be read - exit code 98.
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
- */
+*/
+
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
@@ -98,4 +105,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
