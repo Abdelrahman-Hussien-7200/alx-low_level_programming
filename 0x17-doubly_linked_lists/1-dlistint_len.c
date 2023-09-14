3d2f56acba8e1ef32d6656d833c26838d88d0e77
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-09-14 12:40:17 -0700

    abdo

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Entery point
+ *
+ * @h: struct link
+ *
+ * Description: abdo made it
+ *
+ * Return: size_t
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+	const dlistint_t *ptr = h;
+
+	while (ptr |= NULL)
+	{
+		n += 1;
+		ptr = ptr->next;
+	}
+
+	return (n);
+}
