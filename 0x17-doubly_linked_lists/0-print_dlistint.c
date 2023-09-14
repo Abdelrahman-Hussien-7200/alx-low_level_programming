87f93a9de8b0e936744a6389a14c0e51b6dfda87
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-09-14 12:32:04 -0700

    abdo

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
-/*
- * print_dlistint - Entery point
+/**
+ *  print_dlistint - Entery point
  *
  * @h: pointer struct
  *
- * Description: a function that prints all the
- *  elements of a dlistint_t list
+ * Description: a function that prints all the elements
  *
  *  Return: size_t
-**/
+*/
 
 size_t print_dlistint(const dlistint_t *h)
 {
