bb2432cdecf07cdd4a897e4729c8c6aa2316766f
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-07 00:53:23 +0300

    abdo

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print a hash table in C
+ * @ht: hash table
+ * Return: printed elements
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int in = 0;
+	unsigned int checker = 0;
+	hash_node_t *tem;
+
+	if (!ht)
+		return;
+	printf("{");
+	while (in< ht->size)
+	{
+		tem = ht->array[in];
+		while (tem != NULL)
+		{
+			if (checker == 1)
+				printf(", ");
+			printf("'%s': '%s'", tem->key, tem->value);
+			checker = 1;
+			tem = tem->next;
+		}
+		in++;
+	}
+	printf("}\n");
+}
